5a2f357b9a9a1a939123839e2dbdc4302b6a9cce
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-18 09:00:28 -0500

    Add files via upload

---
+++ date_time/getDateTimeUTC.js
@@ -0,0 +1,60 @@
+// getDateTimeUTC.js
+
+function getDateTimeUTC()
+{
+    let dateTimeUTC = new Date();
+    return dateTimeUTC;
+}
+
+//----//
+
+if (require.main === module)
+{
+    console.log(getDateTimeUTC());
+
+    //-//
+
+    process.stdin.resume();
+    process.stdin.once('data', function()
+    {
+        process.exit();
+    });
+}
+else
+{
+    module.exports = getDateTimeUTC;
+}
+
+//----//
+
+/*
+2024-11-18T13:02:54.400Z
+*/
+
+/*
+YYYY-MM-DDTHH:MM:SS.sssZ
+Year, Month, Day, T, Hours,
+Minutes, Seconds, Milliseconds, Zulu
+
+T
+Separates the date from the time.
+
+Z
+This stands for Zulu time,
+which is another name for UTC,
+Coordinated Universal Time.
+Z means that the time is given
+in UTC with no offset for time zones.
+
+2024-11-18T13:02:54.400Z
+Date: November 18th, 2024
+Time: 13 hours, 2 minutes, 54 seconds, and 400 milliseconds (in UTC)
+Time Zone: UTC (Zulu time)
+*/
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
